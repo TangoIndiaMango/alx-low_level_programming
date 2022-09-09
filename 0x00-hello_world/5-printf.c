0d6f9dca1e211cba2502d0f9a057f468a873946d
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2022-09-08 20:24:57 -0700

    ptintf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - Prints the following line with printf() 
-* Return: 0 as echo value
-*/
+ * main - Prints using printf() command
+ * Return: 0 prints phrase
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,n\");
 	return (0);
 }
