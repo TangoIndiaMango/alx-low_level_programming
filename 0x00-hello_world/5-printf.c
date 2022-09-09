dce54643961f8746ca84568a2290c214dcbc938a
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2022-09-08 19:51:15 -0700

    C is fun (sacarsim)

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Prints the following line with printf() 
+ * Return: 0 as echo value
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
