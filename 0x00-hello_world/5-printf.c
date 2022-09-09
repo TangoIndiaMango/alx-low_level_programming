7912e3d646bdbe05cc7b9ce87a2ffb33e7ef6b3d
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2022-09-08 20:06:51 -0700

    white space

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - Prints the following line with printf() 
- * Return: 0 as echo value
- */
+* main - Prints the following line with printf() 
+* Return: 0 as echo value
+*/
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
