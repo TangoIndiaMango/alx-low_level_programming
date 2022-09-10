7371c956c2774598557261c6a3bd14627579ecf2
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2022-09-09 21:42:06 -0700

    missing character fixed

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - A program that prints all possible combinations of single-digit numbers
+ * main - A program that prints all possible combinations
  *
  * Return: Always 0 (Success)
  */
