b27f2a5a859c5f8e5e9e4ea238b35b6de68057cb
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2022-09-13 23:23:56 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+include "main.h"
+include <stdio.h>
+/**
+ * add - add two int together
+ *
+ * Return: always 0
+ */
+
+int add(int, int)
+{
+	int i;
+	int j;
+
+	sum = i + j;
+
+	printf(sum);
+
+	return (0)
+}
