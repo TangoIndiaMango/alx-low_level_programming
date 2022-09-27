0d1b77bfec7ec17714b3fbfedb89712130a805b0
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2022-09-27 04:28:11 -0700

    pointers

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,7 @@
-#ifndef _HOLDEME_H_
-#define _HOLDEME_H_
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char);
 
 char *_memset(char *s, char b, unsigned int n);
 
@@ -7,11 +9,9 @@ char *memcpy(char *dest, char *src, unsigned int n);
 
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s,char *accept);
+char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int _putchar(char);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(charr **s, char *to);
-
 #endif
