838d2bc2df092ab79d3f99aed43a7a668b014334
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2022-10-04 21:55:35 -0700

    Doc malloc free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - returns a pointer to a newly alllocated space in memeory
+ * @str: pointer to string to be duplicate
+ *
+ * Return: NULL if str is NULL. pointer if success, NULL if memory is not enough
+ */
+
+char *_strdup(char *str)
+{
+	char *mstr;
+	unsigned int len, i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	len = 0;
+
+	while  (str[len] != '\0')
+	{
+		len ++;
+	}
+
+	mstr = malloc(sizeof(char) * (len + 1));
+
+	if (mstr == NLL)
+
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		mstr[i] = str[i];
+	}
+	mstr[len] = '\0';
+	return (mstr);
+}
