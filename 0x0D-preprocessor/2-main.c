33cd796cf99b0150a7b798f242e0895315b1cc4e
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2022-10-10 16:41:45 -0700

    preprocessor C

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print the file compiled from
+ *
+ * Return: File it was compiled from
+ *
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef _SUM_LIKE_MACRO_
+#define _SUM_LIKE_MACRo_
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
