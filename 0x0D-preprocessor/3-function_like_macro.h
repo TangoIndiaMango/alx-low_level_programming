ef5cc3f21c3ffe5dfc92b8fb68de28422dcbea19
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2022-10-10 17:08:51 -0700

    preprocessor C

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,6 +2,6 @@
 #define _FUNCTION_LIKE_MACRO_
 
 
-#define ABS(x) ((x)) < (0) ? ((x) * (-1)) : (x))
+#define ABS(x) ((x) < (0) ? ((x) * (-1)) : (x))
 
 #endif
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,5 +1,5 @@
 #ifndef _SUM_LIKE_MACRO_
-#define _SUM_LIKE_MACRo_
+#define _SUM_LIKE_MACRO_
 
 #define SUM(x, y) ((x) + (y))
 
