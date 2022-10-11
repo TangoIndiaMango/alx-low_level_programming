e5e08d92bfe6c3c35024545f537e10b1b49ae4e1
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2022-10-10 17:00:53 -0700

    struct C

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -27,6 +27,6 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void frr_dog(dog_t *d);
+void free_dog(dog_t *d);
 
 #endif
