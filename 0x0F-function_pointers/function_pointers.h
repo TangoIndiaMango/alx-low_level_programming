b3c7c7cb32387518b4aae91c6028c2ffc60a5066
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2022-10-12 16:06:20 -0700

    funtion pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,2 +1,11 @@
-ear
+#ifndef _FUNCTION_POINTERS_
+#define _FUNCTION_POINTERS_
 
+
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
